3f734252092bb741d907007e1b7b2a14ca2e36e8
Jaman Dedy <jeandedieuam@gmail.com>
2019-12-12 18:55:36 +0200

    Chore(setup testing):
    [Delivers #170257647]

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-import express from "express";
-import connectDB from "./config/db";
-import router from "./routes/app";
+import express from 'express';
+import connectDB from './config/db';
+import router from './routes/app';
 
 const app = express();
 
-//Connect to the db
+// Connect to the db
 connectDB();
 
-//Init Middlware
+// Init Middlware
 
 app.use(express.json({ extended: false }));
 
 app.use(router);
+app.use((req, res) => {
+  res.status(404).send({
+    status: 404,
+    error: 'Page not found'
+  });
+});
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`server up on port ${port}`));
